c34473f61fb1753008423b5d314e5a5d0e001481
vinsofts-cuongnq <cuongnq@vinsofts.net>
2018-10-05 09:10:47 +0700

    sửa lỗi

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Post } from '../post';
+import { Post } from '../Post';
 import { User } from '../user';
 import { MarkdownModule } from 'ngx-markdown';
 
